ed6252a94bde8035638bc601ed5d6ca9df3a6a08
Alex Wollenschlaeger <alexw@panix.com>
2015-03-11 16:17:38 +0000

    initial commit

---
+++ src/test/StateTests.scala
@@ -0,0 +1,23 @@
+package test
+
+import org.scalatest._
+import connectfour._
+
+class StateTests extends FlatSpec with Matchers {
+  
+  "The initial state" should "have Board.NUM_COLS children, all with player YELLOW (assuming RED goes first)" in {
+    val initialState = new State(RED, new Board(), null)
+    initialState.initializeChildren()
+    val children = initialState.getChildren()
+    children.length should be (Board.NUM_COLS)
+  }
+  
+  it should "have children all with player YELLOW, assuming RED goes first" in {
+	  val initialState = new State(RED, new Board(), null)
+	  initialState.initializeChildren()
+	  val children = initialState.getChildren()
+	  for (child <- children) {
+		  child.getPlayer() should be (YELLOW)
+	  }
+  }
+}
