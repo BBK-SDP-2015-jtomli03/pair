136d427cf031016bb8f138df51053ea727932e58
Alex Wollenschlaeger <alexw@panix.com>
2015-03-26 16:22:55 +0000

    removed superfluous comments

--- src/test/AITests.scala
+++ src/test/AITests.scala
@@ -6,12 +6,6 @@ import connectfour._
 class AITests extends FlatSpec with Matchers {
 
   // Suite of tests for the class AI
-  /*
-   * tests needed for:
-   * - AI.createGameTree(s: State, d: Int): Unit
-   * - AI.minimax(ai: AI, s: State): Unit
-   * - AI.getMoves(b: Board): Array[Move]
-   */
   
   "An AI player" should "have at least one possible move on a new board for any d up to 4" in {
     for (i <- 1 to 5) new AI(RED, i).getMoves(new Board()).length should be > (0)
